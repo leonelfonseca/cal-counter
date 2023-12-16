1f83cc188aa64161dcbd9143d41b87c149563f7c
Leonel Fonseca <leonelfonseca@gmail.com>
2023-12-16 10:28:48 +0000

    Starting commit

---
+++ src/main/java/com/calories/calcounter/CalCounterApplication.java
@@ -0,0 +1,13 @@
+package com.calories.calcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calories/calcounter/CalCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calories.calcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
